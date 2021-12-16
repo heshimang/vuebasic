bcb7211b261aa902b1fee9a3765eae1907c6a7c3
heshimang <hsm15031585502@163.com>
2021-12-16 17:08:35 +0800

    组件

---
+++ _单文件组件/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <School />
+  </div>
+</template>
+
+<script>
+import School from './School.vue'
+export default {
+  components: {
+    School
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ _单文件组件/School.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div>学校名称：{{name}}</div>
+    <div>学校地址：{{address}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: '武强中学',
+      address: '武强县'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+ 
